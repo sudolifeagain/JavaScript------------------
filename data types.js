5aa90f7fd1119bc75139e3f9da5762eac3612a78
b00kman <emiliataanjyd@gmail.com>
2024-08-31 20:27:32 +0900

    studying js

---
+++ data types.js
@@ -0,0 +1,14 @@
+/* Data types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName  = "Beau"
+
+myName = 8
+//varは再代入・再宣言が可能　プログラムのすべての範囲で適用される
+
+let ourName = "freeCodeCamp"
+//letはスコープ内でのみ適用される
+
+const pi = 3.14
+// constは定数。代入したものを変更することは不可能。
